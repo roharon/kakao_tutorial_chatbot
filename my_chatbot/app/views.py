1eba3b693f2ea75e083c9848d27db9e6e7d12e7b
roharon <roharon@localhost.localdomain>
2018-01-02 11:40:26 +0900

    Final

--- my_chatbot/app/views.py
+++ my_chatbot/app/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -20,7 +19,7 @@ def message(request):
     json_str = (request.body).decode('utf-8')
     received_json = json.loads(json_str)
     content_name = received_json['content']
-    user_name = received_json['user_key']
+    # user_name = received_json['user_key']
     type_name = received_json['type']
 
     if content_name == "학식":
@@ -59,3 +58,21 @@ def message(request):
                 }
             }
         )
+    else:
+        mess = "이걸 보고계시다면 오류입니다, 개발자에게 알려주세요"
+        if type_name == 'photo':
+            mess = "사진은 기능이 없어요, 버튼을 눌러주세요!"
+        elif type_name == 'video':
+            mess = "영상은 기능이 없어요, 버튼을 눌러주세요!"
+        elif type_name == 'audio':
+            mess = "녹음파일은 기능이 없어요, 버튼을 눌러주세요!"
+
+        return JsonResponse({
+            'message': {
+                'text': mess
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['학식', '내일의 학식', '시간별 학식']
+            }
+        })
